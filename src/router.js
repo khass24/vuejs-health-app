74872bbf78d1388f44a06669d6cf5c4384433cd4
Kyle Hassett <kylehassett72@gmail.com>
2018-10-28 14:30:42 -0500

    updating vue w/ gratitudes

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from './views/Home.vue'
-import About from './views/About.vue'
+
 import Signup from './views/Signup.vue'
 import Login from "./views/Login.vue"
 import Logout from "./views/Logout.vue";
@@ -10,8 +11,8 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: "/", name: "home", component: Home },
-    { path: "/about", name: "about", component: About },
+    { path: "/", name: "root", component: Home },
+
     { path: "/signup", name: "signup", component: Signup },
     { path: "/login", name: "login", component: Login },
     { path: "/logout", name: "logout", component: Logout }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,73 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h2>Your Gratitudes</h2>
+    <div class="index" v-for="gratitude in gratitudes">
+      <h5>{{gratitude.message }}!</h5>
+    </div>
+
+    <div class="add">
+      Today's Gratitude: <input v-model="newGratitude.message">
+      <button v-on:click="addGratitude()">Add Gratitude!</button>
+    </div>
   </div>
 </template>
 
 <style>
+.add {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.index {
+  text-align: center;
+  margin: 20px;
+}
+
+h2 {
+  text-align: center;
+  margin: 30px;
+}
+
+body {
+  font-family: "Palatino Linotype";
+}
+
+
 </style>
 
 <script>
+var axios = require('axios');
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Health App",
+
+      gratitudes: [],
+      newGratitude: { message: ""}
     };
   },
-  created: function() {},
-  methods: {},
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/gratitudes")
+    .then(response => {
+      this.gratitudes = response.data;
+    });
+  },
+  methods: {
+    addGratitude: function() {
+      var params = {
+                    message: this.newGratitude.message
+                    };
+
+      axios
+      .post("http://localhost:3000/api/gratitudes", params)
+      .then(response => {
+        this.gratitudes.push(response.data);
+        this.newGratitude = {message: ""};
+      });
+    }
+  },
   computed: {}
 };
 </script>
